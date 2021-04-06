267e893623f734fef5d334a356dd1d3c335643e8
PJ <peterjan.brone@gmail.com>
2021-04-06 19:58:03 +0200

    Update tsconfig, add semi colons

--- src/dac.ts
+++ src/dac.ts
@@ -1,6 +1,6 @@
 import { IContentCreation, IContentInteraction } from "./types";
 import { Permission } from "skynet-mysky-utils";
-import { DacLibrary } from 'skynet-js'
+import { DacLibrary } from 'skynet-js';
 
 export class ContentRecordDAC extends DacLibrary {
   public constructor(dacUrl: string) {
@@ -8,14 +8,14 @@ export class ContentRecordDAC extends DacLibrary {
   }
   
   public getPermissions(): Permission[] {
-    return []
+    return [];
   }
 
   public async recordCreate(data: IContentCreation): Promise<string> {
-    return await this.connector?.connection.remoteHandle().call("recordCreate", data)
+    return await this.connector?.connection.remoteHandle().call("recordCreate", data);
   }
 
   public async recordInteraction(data: IContentInteraction): Promise<string> {
-    return await this.connector?.connection.remoteHandle().call("recordInteraction", data)
+    return await this.connector?.connection.remoteHandle().call("recordInteraction", data);
   }
 }
